0d85fe9424722320bba9a9ac8873d1df9803300e
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-03-01 16:35:52 +0200

    Add GenericRepository

---
+++ src/Services/BookingClone.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace BookingClone.Domain.Contracts;
+
+public interface IGenericRepository<TEntity, TId> where TEntity : class
+{
+    TEntity Add(TEntity entity);
+    void Delete(TEntity entity);
+    Task<TEntity?> GetById(TId id, CancellationToken ct = default);
+    Task<int> SaveAsync(CancellationToken ct = default);
+    TEntity Update(TEntity entity);
+}
