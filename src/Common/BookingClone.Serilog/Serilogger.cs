31a238f63d4bd2ad61ba96a2f644655166750419
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-03-19 19:40:40 +0200

    Added Elasticsearch Logging

--- src/Common/BookingClone.Serilog/Serilogger.cs
+++ src/Common/BookingClone.Serilog/Serilogger.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace BookingClone.Serilog;
 
@@ -8,11 +10,22 @@ public static class Serilogger
     public static Action<HostBuilderContext, LoggerConfiguration> Configure =>
         (context, configuration) =>
         {
+            string? elasticUrl = context.Configuration.GetConnectionString("Elasticsearch");
+
             configuration
                 .Enrich.FromLogContext()
                 .Enrich.WithMachineName()
                 .WriteTo.Console()
                 .WriteTo.Debug()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUrl!))
+                {
+                    AutoRegisterTemplate = true,
+                    IndexFormat = $"applogs-{context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                    NumberOfShards = 2,
+                    NumberOfReplicas = 1,
+                    FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
+                    EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog
+                })
                 .ReadFrom.Configuration(context.Configuration);
         };
 }
