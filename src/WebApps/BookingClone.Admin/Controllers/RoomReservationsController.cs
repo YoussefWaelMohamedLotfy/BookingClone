5519afc408cce645573552de247518f697466044
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-03-25 02:52:38 +0200

    Completed all CRUD Operations for RoomReservation

--- src/WebApps/BookingClone.Admin/Controllers/RoomReservationsController.cs
+++ src/WebApps/BookingClone.Admin/Controllers/RoomReservationsController.cs
@@ -1,8 +1,10 @@
-﻿using BookingClone.Application.Features.RoomReservationFeatures.Queries.GetAllRoomReservations;
+﻿using BookingClone.Application.Features.RoomReservationFeatures.Commands.AddRoomReservation;
+using BookingClone.Application.Features.RoomReservationFeatures.Commands.DeleteRoomReservation;
+using BookingClone.Application.Features.RoomReservationFeatures.Commands.UpdateRoomReservation;
+using BookingClone.Application.Features.RoomReservationFeatures.DTOs;
+using BookingClone.Application.Features.RoomReservationFeatures.Queries.GetAllRoomReservations;
 using BookingClone.Application.Features.RoomReservationFeatures.Queries.GetRoomReservationById;
-
 using MediatR;
-
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookingClone.Admin.Controllers;
@@ -24,5 +26,44 @@ public sealed class RoomReservationsController : Controller
     {
         var reservation = await _mediator.Send(new GetRoomReservationByIdQuery { ID = id }, ct);
         return View(reservation);
+    }
+
+    public IActionResult Create()
+        => View();
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(AddRoomReservationDto request, CancellationToken ct)
+    {
+        if (!ModelState.IsValid)
+            return View(request);
+
+        var newReservation = await _mediator.Send(new AddRoomReservationCommand { Dto = request }, ct);
+        return RedirectToAction(nameof(Details), new { id = newReservation.ID });
+    }
+
+    public async Task<IActionResult> Edit(int id, CancellationToken ct)
+    {
+        var reservations = await _mediator.Send(new GetRoomReservationByIdQuery { ID = id }, ct);
+        return View(reservations);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(UpdateRoomReservationDto request, CancellationToken ct)
+    {
+        if (!ModelState.IsValid)
+            return View(request);
+
+        await _mediator.Send(new UpdateRoomReservationCommand { Dto = request }, ct);
+        return RedirectToAction(nameof(Details), new { id = request.ID });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id, CancellationToken ct)
+    {
+        await _mediator.Send(new DeleteRoomReservationCommand { ID = id }, ct);
+        return RedirectToAction(nameof(Index));
     }
 }
