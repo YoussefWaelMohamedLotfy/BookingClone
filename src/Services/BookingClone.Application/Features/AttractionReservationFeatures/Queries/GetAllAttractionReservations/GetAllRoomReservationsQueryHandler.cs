86578019c9fdd6102c47b5f02338b81d45cf0dc7
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-03-26 00:48:12 +0200

    Added Controller for AttractionReservations

---
+++ src/Services/BookingClone.Application/Features/AttractionReservationFeatures/Queries/GetAllAttractionReservations/GetAllRoomReservationsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BookingClone.Application.Features.AttractionReservationFeatures.DTOs;
+using BookingClone.Domain.Contracts;
+using MediatR;
+
+namespace BookingClone.Application.Features.RoomReservationFeatures.Queries.GetAllRoomReservations;
+
+internal sealed class GetAllAttractionReservationsQueryHandler : IRequestHandler<GetAllAttractionReservationsQuery, IEnumerable<GetAttractionReservationDto>>
+{
+    private readonly IRoomReservationRepository _roomReservationRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllAttractionReservationsQueryHandler(IRoomReservationRepository repository, IMapper mapper)
+    {
+        _roomReservationRepository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<GetAttractionReservationDto>> Handle(GetAllAttractionReservationsQuery request, CancellationToken cancellationToken)
+    {
+        var reservations = await _roomReservationRepository.GetAll(cancellationToken);
+        return _mapper.Map<List<GetAttractionReservationDto>>(reservations);
+    }
+}
