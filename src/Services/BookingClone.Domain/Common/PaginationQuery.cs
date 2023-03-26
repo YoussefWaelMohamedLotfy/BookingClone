fdde356c0687fe612ef442d8dd3295999e76192b
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-03-27 01:40:34 +0200

    Updated RoomReservation to use Pagination in Admin Dashboard

--- src/Services/BookingClone.Domain/Common/PaginationQuery.cs
+++ src/Services/BookingClone.Domain/Common/PaginationQuery.cs
@@ -2,7 +2,19 @@
 
 public sealed class PaginationQuery
 {
-    public int PageNumber { get; init; } = 1;
+    public int PageNumber { get; init; }
 
-    public int PageSize { get; init; } = 10;
+    public int PageSize { get; init; }
+
+    public PaginationQuery()
+    {
+        PageNumber = 1;
+        PageSize = 10;
+    }
+
+    public PaginationQuery(int pageNumber, int pageSize)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+    }
 }
