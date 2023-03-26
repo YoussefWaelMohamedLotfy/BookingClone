17c2684c39f3bf7e782d388e06a6d280c84cfe80
Youssef Wael Mohamed-Lotfy <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-03-26 16:18:40 +0200

    [Admin] BOOK-18 - AttractionReservation Views (#13)
    * Added Controller for AttractionReservations
    
    * Added index page for AttractionReservations
    
    * Added CRUD Views for AttractionReservation

---
+++ src/Services/BookingClone.Application/Features/AttractionReservationFeatures/Queries/GetAllAttractionReservations/GetAllRoomReservationsQuery.cs
@@ -0,0 +1,8 @@
+﻿using BookingClone.Application.Features.AttractionReservationFeatures.DTOs;
+using MediatR;
+
+namespace BookingClone.Application.Features.RoomReservationFeatures.Queries.GetAllRoomReservations;
+
+public sealed class GetAllAttractionReservationsQuery : IRequest<IEnumerable<GetAttractionReservationDto>>
+{
+}
