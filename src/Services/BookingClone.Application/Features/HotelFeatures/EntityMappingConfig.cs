8e29f4fbb3e225f13febe8173a6a2100c310aaa3
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-03-27 20:47:29 +0200

    Final Touches

--- src/Services/BookingClone.Application/Features/HotelFeatures/EntityMappingConfig.cs
+++ src/Services/BookingClone.Application/Features/HotelFeatures/EntityMappingConfig.cs
@@ -1,26 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using AutoMapper;
-
+﻿using AutoMapper;
 using BookingClone.Application.Features.HotelFeatures.DTOs;
 using BookingClone.Domain.Entities;
 
 namespace BookingClone.Application.Features.HotelFeatures;
-public class EntityMappingConfig:Profile
-{
 
+public class EntityMappingConfig : Profile
+{
     public EntityMappingConfig()
     {
         CreateMap<Hotel, GetHotelsDto>();
         CreateMap<AddHotelDto, Hotel>();
         CreateMap<UpdateHotelDto, Hotel>();
     }
-
-
-
-
 }
