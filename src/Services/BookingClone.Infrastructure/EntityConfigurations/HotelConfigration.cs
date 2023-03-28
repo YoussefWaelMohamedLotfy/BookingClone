dbd9360e7a5b4935a4f645f42a2c13c9ca09778e
NorhanSabry <norhan.sabry98@gmail.com>
2023-03-28 16:24:51 +0200

    ViewsForHotel

--- src/Services/BookingClone.Infrastructure/EntityConfigurations/HotelConfigration.cs
+++ src/Services/BookingClone.Infrastructure/EntityConfigurations/HotelConfigration.cs
@@ -1,4 +1,11 @@
-﻿using BookingClone.Domain.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BookingClone.Domain.Entities;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
---
+++ src/WebApps/BookingClone.Admin/Controllers/HotelController.cs
@@ -0,0 +1,74 @@
+﻿using BookingClone.Domain.Entities;
+using BookingClone.Infrastructure.Data;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BookingClone.Admin.Controllers;
+public class HotelController : Controller
+{
+    BookingDbContext _bookingDbContext = new BookingDbContext();
+    //public IActionResult Index()
+    //{
+    //    return View();
+    //}
+
+    public IActionResult GetHotel()
+    {
+
+        var hotel = _bookingDbContext.Hotels.ToList();
+        return View("GetHotel", hotel);
+    }
+
+
+    public IActionResult CreateHotel()
+    {
+
+        return View();
+    }
+    [HttpPost]
+    public IActionResult CreateHotel(Hotel hotel)
+    {
+
+        _bookingDbContext.Add(hotel);
+        _bookingDbContext.SaveChanges();
+
+        return RedirectToAction("GetHotel");
+    }
+
+
+    public IActionResult UpdateHotel()
+    {
+        return View();
+    }
+
+    [HttpPost]
+
+    public IActionResult UpdateHotel(Hotel hotel)
+    {
+
+        _bookingDbContext.Update(hotel);
+        _bookingDbContext.SaveChanges();
+
+        return RedirectToAction("GetHotel");
+    }
+
+
+    public IActionResult DeleteHotel()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult DeleteCategory(int id)
+    {
+        
+        var DeletedHotel = _bookingDbContext.Hotels.Find(id);
+        _bookingDbContext.Hotels.Remove(DeletedHotel);
+        _bookingDbContext.SaveChanges();
+
+        return RedirectToAction("GetHotel");
+    }
+
+}
