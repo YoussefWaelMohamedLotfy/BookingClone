e845926a45d0b992bdf00822557aa95303b8f8df
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-03-29 23:35:32 +0200

    Added Components for Stays and NotFound

--- src/WebApps/BookingClone/src/app/app-routing.module.ts
+++ src/WebApps/BookingClone/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { StaysComponent } from './Components/stays/stays.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Stays', component: StaysComponent },
+  { path: '', redirectTo: '/Stays', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/WebApps/BookingClone/src/app/app.module.ts
+++ src/WebApps/BookingClone/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { StaysComponent } from './Components/stays/stays.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent,
+    StaysComponent
   ],
   imports: [
     BrowserModule,
