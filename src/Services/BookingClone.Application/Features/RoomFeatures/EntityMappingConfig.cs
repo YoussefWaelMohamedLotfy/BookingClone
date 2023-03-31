72845920be4a3acbc99629ee756fef75866ba7b0
NorhanSabry <norhan.sabry98@gmail.com>
2023-03-31 19:50:59 +0200

    ViewsForRoom

--- src/Services/BookingClone.Application/Features/RoomFeatures/EntityMappingConfig.cs
+++ src/Services/BookingClone.Application/Features/RoomFeatures/EntityMappingConfig.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using BookingClone.Application.Features.RoomFeatures.DTOs;
+using BookingClone.Application.Features.RoomReservationFeatures.DTOs;
+using BookingClone.Domain.Common;
 using BookingClone.Domain.Entities;
 
 namespace BookingClone.Application.Features.RoomFeatures;
@@ -11,5 +13,7 @@ public class EntityMappingConfig : Profile
         CreateMap<Room, GetRoomDto>();
         CreateMap<AddRoomDto, Room>();
         CreateMap<UpdateRoomDto, Room>();
+        CreateMap<PagedList<Room>, PagedList<GetRoomDto>>()
+           .ForMember(x => x.Data, f => f.MapFrom(x => x.Data));
     }
 }
--- src/WebApps/BookingClone.Admin/Controllers/RoomController.cs
+++ src/WebApps/BookingClone.Admin/Controllers/RoomController.cs
@@ -1,4 +1,12 @@
-﻿using MediatR;
+﻿using BookingClone.Application.Features.RoomFeatures.Commands.AddRoom;
+using BookingClone.Application.Features.RoomFeatures.Commands.DeleteRoom;
+using BookingClone.Application.Features.RoomFeatures.Commands.UpdateRoom;
+using BookingClone.Application.Features.RoomFeatures.DTOs;
+using BookingClone.Application.Features.RoomFeatures.Queries.GetAllRooms;
+using BookingClone.Application.Features.RoomFeatures.Queries.GetRoomById;
+
+
+using MediatR;
 
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,9 +19,58 @@ public class RoomController : Controller
     public RoomController(IMediator mediator)
         => _mediator = mediator;
 
-    //public async Task<IActionResult> Index(int pageNumber = 1, int pageSize = 10, CancellationToken ct = default)
-    //{
-    //    var reservations = await _mediator.Send(new GetAllRoomReservationsQuery() { Query = new(pageNumber, pageSize) }, ct);
-    //    return View(reservations);
-    //}
+    public async Task<IActionResult> Index(int pageNumber = 1, int pageSize = 10, CancellationToken ct = default)
+    {
+        var room = await _mediator.Send(new GetAllRoomsQuery() { Query = new(pageNumber, pageSize) }, ct);
+        return View(room);
+    }
+
+
+    public async Task<IActionResult> Details(int id, CancellationToken ct)
+    {
+        var room = await _mediator.Send(new GetRoomByIdQuery { ID = id }, ct);
+        return View(room);
+    }
+
+    public IActionResult Create()
+        => View();
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(AddRoomDto request, CancellationToken ct)
+    {
+        if (!ModelState.IsValid)
+            return View(request);
+
+        var newRoom = await _mediator.Send(new AddRoomCommand { Dto = request }, ct);
+        return RedirectToAction(nameof(Details), new { id = newRoom.ID });
+    }
+
+    public async Task<IActionResult> Edit(int id, CancellationToken ct)
+    {
+        var room = await _mediator.Send(new GetRoomByIdQuery { ID = id }, ct);
+        return View(room);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(UpdateRoomDto request, CancellationToken ct)
+    {
+        if (!ModelState.IsValid)
+            return View(request);
+
+        await _mediator.Send(new UpdateRoomCommand { Dto = request }, ct);
+        return RedirectToAction(nameof(Details), new { id = request.ID });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id, CancellationToken ct)
+    {
+        await _mediator.Send(new DeleteRoomCommand { ID = id }, ct);
+        return RedirectToAction(nameof(Index));
+    }
+
+
+
 }
