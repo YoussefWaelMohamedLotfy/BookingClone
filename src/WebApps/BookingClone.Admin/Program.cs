b5d398fbb65cc4e1d40905c1597937406b753855
salsabeel <salsabeelhatem52@gmail.com>
2023-04-02 20:04:49 +0200

    *HotelReviews View* is done *AttractionReviews View* is done

--- src/WebApps/BookingClone.Admin/Program.cs
+++ src/WebApps/BookingClone.Admin/Program.cs
@@ -44,7 +44,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
-{
+{   
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
