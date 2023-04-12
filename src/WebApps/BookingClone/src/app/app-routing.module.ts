cc1ac060cb473e8bb8476caa7cbe415b648073ab
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-04-12 20:16:21 +0200

    Fix indentation in app.module

--- src/WebApps/BookingClone/src/app/app-routing.module.ts
+++ src/WebApps/BookingClone/src/app/app-routing.module.ts
@@ -8,9 +8,6 @@ import { HotelDetailsComponent } from './Hotels/Components/hotel-details/hotel-d
 import { AllRoomReservationsComponent } from './Reservations/RoomReservations/Components/all-room-reservations/all-room-reservations.component';
 import { AllAttractionReservationsComponent } from './Reservations/AttractionReservations/Components/all-attraction-reservations/all-attraction-reservations.component';
 import { CityComponent } from './City/city/city.component';
-// import { CityDetailsComponent } from './City/city-details component/city-details.component';
-import { CountryComponent } from './Country/country/country.component';
-import { CountryDetailsComponent } from './Country/country-details/country-details.component';
 import { ContinentComponent } from './Continent/continent/continent.component';
 import { CityDetailsComponent } from './City/city-details/city-details.component';
 import { EgyptCitiesComponent } from './City/egypt-cities/egypt-cities.component';
@@ -18,16 +15,16 @@ import { EgyptCitiesComponent } from './City/egypt-cities/egypt-cities.component
 const routes: Routes = [
   { path: '', redirectTo: '/Stays', pathMatch: 'full' },
   { path: 'Stays', component: StaysComponent },
-  {path:'egyptcities',component:EgyptCitiesComponent},
-   {path:'City',component:CityComponent},
-  {path:'CityDetails/:CountryID',component:CityDetailsComponent},
-  {path:'Continent',component:ContinentComponent},
+  { path: 'egyptcities', component: EgyptCitiesComponent },
+  { path: 'City', component: CityComponent },
+  { path: 'CityDetails/:CountryID', component: CityDetailsComponent },
+  { path: 'Continent', component: ContinentComponent },
   { path: 'Room/Reservations', component: AllRoomReservationsComponent },
   { path: 'Attraction/Reservations', component: AllAttractionReservationsComponent },
   { path: 'Hotels', component: HotelsComponent },
   { path: 'HotelDetails/:id', component: HotelDetailsComponent },
   { path: '', redirectTo: '/Room/Reservations', pathMatch: 'full' },
-  {path:'Continent/:id',component:ContinentComponent},
+  { path: 'Continent/:id', component: ContinentComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
